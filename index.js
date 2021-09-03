5102a54bb002ce5dbfc416c39b6f829f9f8ca327
alves-Moises <moisesingo_ra@hotmail.com>
2021-09-02 23:57:11 -0300

    update: initi

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const qrcode = require('qrcode-terminal');
-
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+//session
+const fs = require('fs');
+const SESSION_FILE_PATH = './session/session.json';
+
+//load session files
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH)
+}
+
+const client = new Client({
+    session: sessionData
 });
+// client.on('qr', qr => {
+//     qrcode.generate(qr, {small: true});
+// });
+
+//saving autenticated session
+client.on('authenticated', (session)=>{
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) =>{
+        if (err){
+            console.err(`err: {err}`)
+        }
+    })
+
+})
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-Client.on('message', message => {
-    console.log(message.body)
 
-    if(message.body == 'ping') {
-        message.reply('pong')
-    }
-    })
+client.on('message', message => {
+   if(messageLower.body == 'ping') {
+    message.reply('pong!')
+   }
+
+})
+
+
 
 client.initialize();
---
+++ math.js
@@ -0,0 +1,21 @@
+function math(a, b, signal){
+    switch(signal) {
+        case '+':
+            console.log('Math, sum')
+            return a + b;
+            
+        case '-':
+            console.log('Math, sub')
+            return a - b;
+            
+        case '*':
+            console.log('Math, mult')
+            return a * b;   
+            
+            case '/':
+                console.log('Math, div')
+                return a / b;
+    }
+}
+
+
